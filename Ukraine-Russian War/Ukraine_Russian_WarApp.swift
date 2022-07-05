e06a3b17e199368c0aa367d396a4b56dbb6de63d
LiliyaVelboy <liliya142003@gmail.com>
2022-07-05 09:49:47 +0300

    Initial Commit

---
+++ Ukraine-Russian War/Ukraine_Russian_WarApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ukraine_Russian_WarApp.swift
+//  Ukraine-Russian War
+//
+//  Created by Лілія on 05.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct Ukraine_Russian_WarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
